79d1a2e52aee53d82b20897522ad0e16e173460e
Leja Podobnik <leja123@users.noreply.github.com>
2021-07-07 20:05:24 +0200

    switch

--- main.c
+++ main.c
@@ -16,10 +16,34 @@ int main()
     printf("\n");
     printf("\n");
     printf("\n");
+
+    char operation;
+    int choice;
+    printf("Choose an operation:\n");
     printf("\n");
+    printf("1 .Addition\n");
+    printf("2. Subtraction\n");
+    printf("3. Multiplication\n");
+    printf("4. Division\n");
+    printf("\n");
+    scanf("%c", &operation);
 
-
-
+    switch(operation) {
+        case '1':
+            printf("Addition");
+            break;
+        case '2':
+            printf("Subtraction");
+            break;
+        case '3':
+            printf("Multiplication");
+            break;
+        case '4':
+            printf("Division");
+            break;
+        default:
+            printf("Wrong choice");
+    }
 
 
     return 0;
