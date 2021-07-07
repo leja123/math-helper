4e65c7fc6bce6b82a3e01b637d0e192731467ca7
Leja Podobnik <leja123@users.noreply.github.com>
2021-07-07 19:40:18 +0200

    initial setup

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\n");
+    printf("                                  --------------------------------------------------------\n");
+    printf("                                  |               Welcome to MATH HELPER!                 |\n");
+    printf("                                  --------------------------------------------------------\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("This program will help you do your math homework.\n");
+    printf("It can do basic and advanced math.\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+
+
+
+
+
+    return 0;
+}
